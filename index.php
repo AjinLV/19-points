0071e8e4e9543d262b9f7c58ffc4324e4e2806fe
AjinLV <85498560+AjinLV@users.noreply.github.com>
2021-08-04 12:28:27 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![end if]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![end if]-->
+
+<head>
+	<meta charset="utf-8">
+	<title>Prakse</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specifc Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Stylessheets -->
+	<link rel="stylesheet" href="assets/css/style.css" />
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="assets/img/favicon.ico">
+</head>
+<body>
+<div class="container">
+  <div class="row justify-content-start">
+    <div class="col-1">
+    <img src="assets/img/BlackLogo 3.jpg">
+    </div>
+      <div class="col-1">
+      
+      </div>
+      <div class="col-1 header-text">
+      Ideja
+      </div>
+      <div class="col-2 header-text">
+      Piesaki Sapni
+      </div>
+      <div class="col-1 header-text">
+      Uzņēmejiem
+      </div>
+      <div class="col-2 header-text">
+      Sapņu Banka
+      </div>
+      <div class="col-1 header-text">
+      Sapņus Atbalsta
+      </div>
+      <div class="col-2 header-text">
+      99 Iedvesmas stāsti
+      </div>
+      <div class="col-1 header-text">
+      LV 
+      </div>
+  </div>
+
+
+  <div class="row justify-content-center">
+    <div class="col-1">
+    <img src="assets/img/Sparkle Video 1.jpg">     
+    </div>
+  </div>
+
+  <div class="container my-container">
+        <div class="row mid-text">
+            <div class="col-md-4 col-sm-6 my-col">
+            
+            </div>
+        </div>
+        <div class="row justify-content-between align-items-stretch mid-text">
+            <div class="col-3 mid-text order-md-12">
+                
+            </div>
+            <div class="col-6 offset-md-2 mid-text align-self-start order-md-2">
+            SparkleHeart ir biedrība, kas radīta, lai palīdzētu cilvēkiem piepildīt pat pārdrošākos sapņus. Šeit satiekas sapņotāji un viņu atbalstītāji - uzņēmumi, kas sniedz palīdzīgu roku, lai kopā pasauli padarītu par labāku vietu mums visiem.Mēs esam gatavi palīdzēt Tev piepildīt sapni. Kā pamats iedvesmai - tiek radīti arī 99 stāsti par Latvijas sievietēm.
+            </div>
+            <div class="col-3 my-col order-md-12">
+                
+            </div>
+            <div class="col-md-4 col-sm-6 my-col">
+                
+            </div>
+        </div>
+    </div>
+
+
+<div class="flex-container ">
+  <div class="flex-item">
+    <img src="assets/img/Zanda_1.jpg" alt="">
+  </div>
+  <div class="flex-item">
+    <img src="assets/img/Sibilla_4.jpg" alt="">
+  </div>
+  <div class="flex-item flex-column">
+  <img src="assets/img/Brinuma_meita-15.jpg" alt="">
+  <img src="assets/img/Alina_1.jpg" alt="">
+</div>
+  <div class="flex-item">
+    <img src="assets/img/Anda_2.jpg" alt="">
+  </div>
+</div>
+        <div class="row justify-content-between align-items-stretch help-text">
+            <div class="col-2 offset-md-2 mid-text align-self-start order-md-2">
+            
+            </div>
+            <div class="col-4 offset-md-3 help-text align-self-start order-md-2">
+            VĒLIES PIEPILDĪT SAVU SAPNI?
+            </div>
+            <div class="col-2 offset-md-2 mid-text align-self-start order-md-2">
+            
+            </div>
+            <div class="col-4 offset-md-3 help-text align-self-end order-md-2">
+            VĒLIES PALĪDZĒT KĀDAM PIEPILDĪT SAPNI?
+            </div>
+            <div class="col-2 offset-md-2 help-text align-self-end order-md-2">
+            
+            </div>
+        </div>
+        <div class="row justify-content-between align-items-stretch help-text">
+            <div class="col-2 offset-md-2 help-button align-self-start order-md-2">
+            
+            </div>
+            <div class="col-4 offset-md-2 help-button align-self-start order-md-2">
+            Sapņotājiem
+            </div>
+            <div class="col-4 offset-md-2 help-button align-self-start order-md-2">
+            Uzņēmējiem
+            </div>
+            <div class="col-2 offset-md-2 help-button align-self-start order-md-2">
+            
+            </div>
+        </div>
+
+
+  <div class="row justify-content-end">
+    <div class="footer">
+      <p>Temporary footer</p>
+    </div>
+
+	<!-- JavaScript -->
+	<script src="js/theme.js"></script>
+</body>
+</html>
